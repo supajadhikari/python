b42e3ac2f917396c7fcae03e966d9293554130e6
Supaj Adhikari <adhikarisupaj1@gmail.com>
2025-12-18 09:29:05 +0545

    Add files via upload
    done

---
+++ day1.py
@@ -0,0 +1,74 @@
+print("hello world")
+print("hello world")
+num1=10
+print(num1)
+#snake_case
+first_name = "supaj"
+print(first_name)
+#in python we dont need to write type of variable like string or number 
+#comment
+'''print("file")'''
+"""print("hello")"""
+# '''''' """""" this are used for the comment 
+#int - basic number but without decimal point value 
+#float - decimal number - > 1.99, 1.00
+#complex - > scientific calculations -> 3i +  1 
+#array/list -> collection of mutable values 
+a_variable = "a"
+print(a_variable)
+#mutable change once created 
+#immutable -> cannot be changed once created 
+#tuple -> collection of immutable values
+#set -> collection of unique values
+#dict -> collection of keys and value pair 
+#string 
+name = "hello spj"
+name = 'hy spj'
+print(name) #it will print hy spj 
+details = """
+hy 
+this is a file 
+"""
+#methods os strings ->
+print(name + details) 
+name ='it\'s me spj'
+print(name)
+num2 = 100
+print(num1+num2)
+#to declare name and the variable we need comma 
+print(name, num1)
+float
+num3 = 10.78
+print(num3 + num1)
+
+#complex
+j=10
+comp = 1+3j
+print(comp)
+
+#list
+list1 = [1,2,3, "spj" , 4 , 5 ,5]
+print(list1)
+
+#tuple1
+
+tuple1 = (1,2,3,4,5)
+print(tuple1)
+
+#set 3
+set3= (1,2,3,4,5,6)
+print(set3)
+
+#dict1
+
+dict1 = {
+    "first name" : "Spj",
+    "last name" : "supaj",
+    "college" : "kist",
+    "age" : 27,
+    "salary" : 50000, 
+}
+print(dict1)
+
+
+
