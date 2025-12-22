5a6ed19617a343611185de68f966b0c142e7a11e
Supaj Adhikari <adhikarisupaj1@gmail.com>
2025-12-22 09:30:08 +0545

    Update tuple.py
    done

--- tuple.py
+++ tuple.py
@@ -1,13 +1,26 @@
-#2. Tuple -> collection of immutable items 
-
-lst = [9,8,7,6,5,4]
+# Initial list
+lst = [9, 8, 7, 6, 5, 4]
 print(lst)        # [9, 8, 7, 6, 5, 4]
+
+# Access element at index 2
 print(lst[2])     # 7
+
+# Update element at index 2
 lst[2] = 10
 print(lst)        # [9, 8, 10, 6, 5, 4]
-list2 = list(tup)   
-print(list2)
-list2.extend([3,2,1])
-print(list2)
+
+# Convert list to tuple
+tup = tuple(lst)
+print(tup)        # (9, 8, 10, 6, 5, 4)
+
+# Convert tuple back to list
+list2 = list(tup)
+print(list2)      # [9, 8, 10, 6, 5, 4]
+
+# Extend list with new elements
+list2.extend([3, 2, 1])
+print(list2)      # [9, 8, 10, 6, 5, 4, 3, 2, 1]
+
+# Convert back to tuple
 tup = tuple(list2)
-print(tup)
+print(tup)        # (9, 8, 10, 6, 5, 4, 3, 2, 1)
