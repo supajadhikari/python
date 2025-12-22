cb016a13e5634f38f746b6172f89f3c6baec14f5
Supaj Adhikari <adhikarisupaj1@gmail.com>
2025-12-22 09:29:32 +0545

    Update dict1.py
    done

--- dict1.py
+++ dict1.py
@@ -4,3 +4,19 @@ dict1 = {
     "city" : "kathmandu"
 }
 
+#list 
+print(dict1.values())
+print(dict1.keys())
+print(dict1.items())
+
+#keys 
+for i in dict1.keys():
+    print(i)
+    
+for i in dict1.values():
+    print(i)
+
+for keys, values in dict1.items():
+    print(f"(keys) : (value)")
+    
+    
